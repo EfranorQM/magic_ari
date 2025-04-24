bd2c57aceebd6eda3875327671e32d0bcd5e175f
EfranorQM <andersonefrainquma@gmail.com>
2025-04-23 22:19:11 -0500

    Arreglo

--- src/pages/Flores.tsx
+++ src/pages/Flores.tsx
@@ -1,5 +1,5 @@
 // src/pages/Flores.tsx
-import React, { useState, useMemo } from "react";
+import { useState, useMemo } from "react";
 import { Link } from "react-router-dom";
 import { floresDestacadas } from "../constants/carruselData";
 
